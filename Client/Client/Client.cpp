58358c685622a5e7c94699851eb83438c163a6e3
theDarkForce <theDarkForce@aliyun.com>
2016-07-14 18:40:07 +0800

    add a sample client/server program

--- Client/Client/Client.cpp
+++ Client/Client/Client.cpp
@@ -1,9 +1,23 @@
 #include "uv.h"
 #include<iostream>
 
+void on_write(uv_write_t* req, int status)
+{
+	if (NULL != req)
+	{
+		delete req;
+	}
+}
+
 void OnConnect(uv_connect_t * conn, int flags)
 {
 	std::cout << "connet to server";
+	uv_write_t *req = new uv_write_t();
+	uv_buf_t buf;
+	char* base = new char[100];
+	strncpy(base, "sjw test", sizeof("sjw test"));
+	buf = uv_buf_init(base, 100);
+	uv_write(req, conn->handle, &buf, 1, on_write);
 }
 
 int main()
@@ -11,8 +25,8 @@ int main()
 	uv_tcp_t* client = new uv_tcp_t();
 
 	sockaddr_in addr;
-	uv_ip4_addr("192.168.1.122", 1122, &addr);
-
+	uv_ip4_addr("127.0.0.1", 1122, &addr);
+	uv_tcp_init(uv_default_loop(), client);
 	uv_connect_t conn;
 	uv_tcp_connect(&conn, client, (sockaddr*)&addr, OnConnect);
 	uv_run(uv_default_loop(), UV_RUN_DEFAULT);
--- Server/Server/Server.cpp
+++ Server/Server/Server.cpp
@@ -1,9 +1,32 @@
 #include "uv.h"
 #include<iostream>
 
+void on_alloc(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf)
+{
+	char* base = new char[10000];
+	*buf = uv_buf_init(base, 10000);
+}
+
+void on_read(uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf)
+{
+	if (nread > 0)
+	{
+		std::cout << buf->base;
+	}
+}
+
 void on_connection(uv_stream_t* server, int status)
 {
 	std::cout << "client come in";
+	if (-1 == status)
+	{
+		return;
+	}
+
+	uv_tcp_t* client = new uv_tcp_t();
+	uv_tcp_init(uv_default_loop(), client);
+	uv_accept(server, (uv_stream_t*)client);
+	uv_read_start((uv_stream_t*)client, on_alloc, on_read);
 }
 
 int main()
@@ -12,7 +35,7 @@ int main()
 	uv_tcp_init(uv_default_loop(), server);
 
 	sockaddr_in addr;
-	uv_ip4_addr("192.168.1.122", 1122, &addr);
+	uv_ip4_addr("127.0.0.1", 1122, &addr);
 
 	uv_tcp_bind(server, (sockaddr*)&addr, 0);
 
